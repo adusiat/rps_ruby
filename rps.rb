32cc10ef86065ed0f95853d67555ffbe8d3664ad
Raghu Betina <rbetina@gmail.com>
2016-10-12 14:26:37 -0500

    Extracted some logic into methods

--- rps.rb
+++ rps.rb
@@ -2,38 +2,72 @@
 human_move = " "
 human_wins = 0
 computer_wins = 0
+ties = 0
+computer_options = ["rock", "paper", "scissors"]
 
-while human_move.downcase != "quit"
-
-  computer_options =["rock","paper","scissors"]
-  human_options = ["rock","paper","scissors","r","p","s","1","2","3"]
-
-  puts "Welcome to Rock, Paper, Scissors!"
-
-  puts "Your move: rock, paper, or scissors?"
-
-  human_move = gets.chomp.downcase #downcasing input eliminates case sensitivity
-
-  if human_options.include?(human_move) == false
-    puts  "That doesn't appear to be a valid move..."
+def normalize_move(move_input)
+  case move_input.downcase
+  when "rock", "r", "1"
+    move = "rock"
+  when "paper", "p", "2"
+    move = "paper"
+  when "scissors", "s", "3"
+    move = "scissors"
+  when "quit", "q", "exit", "stop", "done"
+    move = "quit"
   else
-    computer_move = computer_options.sample
-    puts computer_move
-    if ((human_move[0] == "r" or "1") and computer_move == "scissors")  or ((human_move[0] == "p" or "2") and computer_move == "rock") or ((human_move[0] == "s" or "3") and computer_move == "paper")
-      puts "You Win!"
-      human_wins = human_wins + 1
-    else
-      if human_move[0] == computer_move[0]
-        puts "It's a tie!"
-      else
-        if human_options.include?(human_move)
-          puts "You Lose!"
-          computer_wins = computer_wins + 1
-        else
-        end
-      end
-    end
+    move = "invalid"
   end
+
+  return move
 end
 
-puts "Human - " + human_wins.to_s + " Computer - " + computer_wins.to_s
+def outcome(human, computer)
+  if human == computer
+    result = "tie"
+  elsif (human == "rock" && computer == "scissors") ||
+        (human == "paper" && computer == "rock") ||
+        (human == "scissors" && computer == "paper")
+    result = "win"
+  else
+    result = "lose"
+  end
+
+  return result
+end
+
+puts "Welcome to Rock, Paper, Scissors!"
+
+while human_move != "quit"
+  puts
+  puts "Your move: rock, paper, or scissors?"
+
+  input_from_user = gets.chomp
+  human_move = normalize_move(input_from_user)
+
+  case human_move
+  when "invalid"
+    puts "That doesn't appear to be a valid move..."
+  when "quit"
+    puts "Okay, thanks for playing!"
+  else
+    puts "You played #{human_move}."
+
+    computer_move = computer_options.sample
+    puts "The computer played #{computer_move}."
+
+    case outcome(human_move, computer_move)
+    when "win"
+      puts "You Win!"
+      human_wins = human_wins + 1
+    when "tie"
+      puts "It's a tie!"
+      ties = ties + 1
+    when "lose"
+      puts "You Lose!"
+      computer_wins = computer_wins + 1
+    end
+
+    puts "Human - #{human_wins}; Computer - #{computer_wins}; Ties - #{ties}"
+  end
+end
